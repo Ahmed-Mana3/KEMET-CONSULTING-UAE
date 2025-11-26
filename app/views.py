634a4dc13b9983a36b3a3c012f33caa25d4ceb0c
am537@Ahmed_Medhat <you@example.com>
2025-11-26 21:44:24 +0200

    website done

--- app/views.py
+++ app/views.py
@@ -33,3 +33,12 @@ def home_view(request):
     }
 
     return render(request, "index.html", context)
+
+
+def page_not_found(request, exception):
+    """Custom 404 page"""
+    return render(request, '404.html', status=404)
+
+def server_error(request):
+    """Custom 500 page"""
+    return render(request, '500.html', status=500)
--- config/urls.py
+++ config/urls.py
@@ -1,37 +1,31 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.contrib.sitemaps.views import sitemap
-from .sitemaps import StaticViewSitemap
+from django.http import HttpResponse
 
-sitemaps = {
-    "static": StaticViewSitemap,
-}
-
+def robots_txt(request):
+    content = """User-agent: *
+Allow: /
+Disallow: /admin/
+Disallow: /media/
+Sitemap: https://kemetvision.com/sitemap.xml
+"""
+    return HttpResponse(content, content_type='text/plain')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls")),
-        path("robots.txt", TemplateView.as_view(
-        template_name="robots.txt", content_type="text/plain"
-    )),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
+    path('robots.txt', robots_txt),
+]
+
+# Serve media & static in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom error handlers
+handler404 = 'app.views.page_not_found'
+handler500 = 'app.views.server_error'
